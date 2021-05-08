875a14e7a5fcec0d1e6a6c3eaa58d7a6fb1665c0
Emily Jewik <emilyjewik@gmail.com>
2021-05-07 19:17:21 -0700

    add simple functions to test linter

--- btns.js
+++ btns.js
@@ -1,40 +1,11 @@
-export { customAlert, customConfirm };
-
-function customAlert() {
-    // weird tabbing for linter testing
-        let alertDialog = document.getElementById('alertDialog');
-    let alertBtn = document.getElementById('alertBtn');
-            alertBtn.addEventListener('click', function () {
-        alertDialog.showModal();
-
-        // large white space for linter testing
-
-
-
-
-
-
-
-
-    });
+function add(a, b) {
+    // whitespace testing for linter
+    let result=a+b;
+    return result;
 }
 
-function customConfirm() {
-    // no whitespace for linter testing 
-    var confirmDialog=document.getElementById('confirmDialog');
-    var confirmButton=document.getElementById('confirmBtn');
-    let cancelButtonDialog=document.getElementById('confirmCancelBtn');
-    let confirmButtonDialog = document.getElementById('confirmConfirmBtn');
-
-    confirmButton.addEventListener('click', function () {
-        confirmDialog.showModal();
-    });
-
-    confirmButtonDialog.addEventListener('click', function () {
-        confirmButtonDialog.value = 'confirmed';
-    });
-
-    cancelButtonDialog.addEventListener('click', function () {
-        cancelButtonDialog.value = 'canceled';
-    });
+function multiply(a, b) {
+    // indentation testing for linter
+            let result = a * b;
+    return result;
 }
