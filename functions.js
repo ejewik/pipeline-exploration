a887dfa540521d9230d1ddb52a77f4453f5466c7
Emily Jewik <emilyjewik@gmail.com>
2021-05-08 16:46:07 -0700

    Remove else if redundant test

--- functions.js
+++ functions.js
@@ -1,27 +1,20 @@
 function unreachableStatement(a, b) {
     // testing unreachable code for the linter
-    //let result = a + b;
-    return a;
+    let result = a + b;
+    return result;
     console.log("This is unreachable");
 }
 
 function redundantElseIf(a, b) {
-    // testing redunant else if for linter
+    // testing redunant parameter
     if (a) {
         return a;
-    } else if (b) {
-        return b;
-    } else if (b) {
-        return a;
-    }
+    } 
 }
 
 function unusedVars() {
     let str = "This is unused.";
 }
 
-function mixedSpacesAndTabs() {
-    let str1 = "This is indented by tab.";
-    let str2 = "This is indented by spaces.";
-    return str1;
-}
+let foo = unreachableStatement(1, 1);
+
